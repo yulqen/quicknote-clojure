dd281b0d5cb9d84184788dac5cf18a698b92f3d7
Matthew Lemon <y@yulqen.org>
2025-03-27 16:11:16 +0000

    wip: utilises the clipboard

--- src/yulqen/quicknote.clj
+++ src/yulqen/quicknote.clj
@@ -1,6 +1,18 @@
 (ns yulqen.quicknote
   (:require [net.cgrand.enlive-html :as html])
+  (:import [java.awt.datatransfer DataFlavor])
   (:gen-class))
+
+(defn
+ clipboard-text
+ []
+ (try
+  (.getTransferData
+   (.getContents
+    (.getSystemClipboard (java.awt.Toolkit/getDefaultToolkit))
+    nil)
+   (DataFlavor/stringFlavor))
+  (catch java.lang.NullPointerException e nil)))
 
 (defn greet
   "Callable entry point to the application."
@@ -8,7 +20,11 @@
   (println (str "Hello, " (or (:name data) "World") "!")))
 
 (defn get-title-text [url]
-  (first (:content (first (html/select (html/html-resource (java.net.URL. url)) [:title])))))
+  (try
+    (first (:content (first (html/select (html/html-resource (java.net.URL. url)) [:title]))))
+    (catch java.net.MalformedURLException e
+      (do
+        (println "Not a URL")) nil)))
 
 (defn format-markdown-link
   "Formats text and url as a markdown link"
@@ -18,8 +34,11 @@
 (defn url-to-markdown [url]
   (format-markdown-link (get-title-text url) url))
 
-(defn append-to-file [path url]
-  (spit path (apply str "- " (url-to-markdown url) "\n") :append true))
+(defn append-to-file []
+  (let [url (clipboard-text)
+        path "/home/lemon/Documents/Notes/quicknote.md"]
+    (spit path (apply str "- " (url-to-markdown url) "\n") :append true)))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
