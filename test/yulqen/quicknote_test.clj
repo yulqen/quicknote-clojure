3349ec5419126893fa015d6d0197eb1aa2aec7ca
Matthew Lemon <y@yulqen.org>
2025-03-28 12:24:36 +0000

    fixed error (again) in deps and added working test

--- test/yulqen/quicknote_test.clj
+++ test/yulqen/quicknote_test.clj
@@ -7,3 +7,8 @@
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 1 1))))
+
+(deftest markdown-link
+  (testing "Ensure text can be converted to markdown."
+    (is (format-markdown-link "Yonkers" "https://example.com")
+        "- [Yonkers](https://example./com")))
